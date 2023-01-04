349814b6d9ba37250be783b7f77392d17d5f64ac
AlineOdr <muhametrahimova-alina.m@yandex.ru>
2023-01-05 02:30:04 +0300

    docstring and mupu edit2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
-    '''Вычисляет урон.'''
+    """Вычисляет урон."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -16,7 +16,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
-    '''Вычисляет защиту.'''
+    """Вычисляет защиту."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -27,7 +27,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
-    '''Вычисляет специальное умение.'''
+    """Вычисляет специальное умение."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость'
                 f'{80 + 25}»')
@@ -39,7 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
-    '''Результат.'''
+    """Результат."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -65,7 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    '''Выбор персонажа.'''
+    """Выбор персонажа."""
     approve_choice = None
     char_class: str = ''
     while approve_choice != 'y':
